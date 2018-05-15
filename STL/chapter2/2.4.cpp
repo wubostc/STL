5febc1c2dcd709384a7024a077c3c23293425903
wubo <913721086@qq.com>
2018-05-15 21:17:48 +0800

    update 2.4

--- STL/chapter2/2.4.cpp
+++ STL/chapter2/2.4.cpp
@@ -7,9 +7,22 @@
 
 using namespace std;
 
+void insert_sorted(vector<string> &v, const string &word){
+  auto insert_pos = std::lower_bound(begin(v), end(v), word);
+  v.insert(insert_pos, word);
+}
+
 int main(){
   vector<string> v{"some", "random", "words", "without", "order", "aaa", "yyy"};
-  assert(!is_sorted(begin(v), end(v)));
+  assert(false == is_sorted(begin(v), end(v)));
 
   sort(begin(v), end(v));
+
+  assert(true == std::is_sorted(begin(v), end(v)));
+
+  ::insert_sorted(v, "foobar");
+  ::insert_sorted(v, "zzz");
+
+
+  assert(true == std::is_sorted(begin(v), end(v)));
 }
